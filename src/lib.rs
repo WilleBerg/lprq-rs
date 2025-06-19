f34d4800a93e836436de0852fdc34f3447c5a677
William <william.berg2@gmail.com>
2025-06-19 12:06:51 +0200

    Added sync

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,7 @@ pub struct LPRQueue<'a, E, const N: usize = 256> {
     next_thread_id: AtomicUsize,
     hps: UnsafeCell<HazardPointerArray<'a, haphazard::Global, N>>,
 }
+unsafe impl<E> Sync for LPRQueue<'_, E>{}
 
 fn is_bottom<T>(value: *const T) -> bool {
     (value as usize & 1) != 0
